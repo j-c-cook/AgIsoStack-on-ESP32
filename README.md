668d482abae0ad6b69e8d02d283d7bc1abc52565
Jack Cook <jack-c-cook@protonmail.com>
2025-01-18 16:36:39 -0600

    Successful mounting of TWAI

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # AgIsoStack++ on ESP Platform
 
-See the Espressif IDF getting started guide for setting up the build environment on either Windows, Linux or MacOS [here](https://docs.espressif.com/projects/esp-idf/en/stable/esp32/get-started/index.html#manual-installation).
+See the Espressif IDF [Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/stable/esp32/get-started/index.html#manual-installation) for setting up the build environment on either Windows, Linux or MacOS.
+
+## How it works
+
+The `iso-on-esp` module is marked as a required component in `main`s [CMakeLists.txt](./main/CMakeLists.txt).
 
 ## Build
 
--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,38 @@
+#include <isobus/hardware_integration/can_hardware_plugin.hpp>
 #include <isobus/hardware_integration/available_can_drivers.hpp>
 #include <isobus/hardware_integration/can_hardware_interface.hpp>
-#include <isobus/hardware_integration/twai_plugin.hpp>
+#include <isobus/hardware_integration/socket_can_interface.hpp>
+#include <isobus/isobus/can_general_parameter_group_numbers.hpp>
+#include <isobus/isobus/can_network_configuration.hpp>
 #include <isobus/isobus/can_network_manager.hpp>
+#include <isobus/isobus/can_parameter_group_number_request_protocol.hpp>
+#include <hal/twai_types.h>
+#include <driver/twai.h>
+#include <esp_log.h>
+#include <memory>
+
+const char * tag = "esp-agisostack-ex";
 
 extern "C" void app_main()
 {
-    printf("Hello world\n");
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_250KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    std::shared_ptr<isobus::CANHardwarePlugin> canDriver = nullptr;
+    canDriver = std::make_shared<isobus::TWAIPlugin>(&g_config, &t_config, &f_config);
+
+    isobus::CANHardwareInterface::set_number_of_can_channels(1);
+	isobus::CANHardwareInterface::assign_can_channel_frame_handler(0, canDriver);
+
+	if ((!isobus::CANHardwareInterface::start()) || (!canDriver->get_is_valid()))
+	{
+	    ESP_LOGE(tag, "Failed to start hardware interface. The CAN driver might be invalid.");
+		return;
+	} else
+	{
+	   ESP_LOGI(tag, "Successful start of hardware interface!");
+	}
+
+	// See https://github.com/Open-Agriculture/AgIsoStack-plus-plus/tree/main/examples and adapt as you see fit.
 }
