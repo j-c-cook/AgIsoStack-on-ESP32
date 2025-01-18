eb6760a255e58e62916f0b0e6229453a32fbf966
Jack Cook <jack-c-cook@protonmail.com>
2025-01-18 13:18:56 -0600

    Minimal build with includes functional

---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <isobus/hardware_integration/available_can_drivers.hpp>
+#include <isobus/hardware_integration/can_hardware_interface.hpp>
+#include <isobus/hardware_integration/twai_plugin.hpp>
+#include <isobus/isobus/can_network_manager.hpp>
+
+extern "C" void app_main()
+{
+    printf("Hello world\n");
+}
